842fe80f5c0c699afa1f57ff0892099884308f51
fluffy-marmot <10621013+fluffy-marmot@users.noreply.github.com>
2026-07-06 20:41:37 -0700

    implement most arithmetic instructions

--- chip8.c
+++ chip8.c
@@ -7,7 +7,6 @@
 // memcpy, memset
 #include <string.h> 
 
-
 uint8_t FONT_SPRITES[] = {
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -99,7 +98,7 @@ fetch_instruction()
     return instruction;
 }
 
-void
+bool
 decode_instruction(uint16_t instruction)
 {
     // breakup the two instruction bytes into four "nibbles" (four half-bytes)
@@ -109,34 +108,99 @@ decode_instruction(uint16_t instruction)
     uint8_t n  = (uint8_t) ((instruction & 0x000F)      );  // a 4-bit number
 
     switch (op) {
-        case 0x00: // clear display, among other things
-            if (instruction == 0x00E0) {
-                display_clear();
+    case 0x00: 
+        if (instruction == 0x00E0) {            // 00E0 clear display instruction
+            display_clear();
+        } else if (instruction == 0x00EE) {     // 00EE instruction to return from subroutine
+            if (CPU->stack_len == 0) {
+                printf("Got subroutine return instruction when stack empty\n");
+                return 0;
             }
+            CPU->PC = CPU->stack[--CPU->stack_len];
+        }
+        break;
+    case 0x01: // 1NNN jump instruction, using other 12 bits as memory address
+        CPU->PC = instruction & 0x0FFF;
+        break;
+    case 0x02: // 2NNN subroutine instruction, similar to 1NNN but push current address on stack
+        if (CPU->stack_len == STACK_SIZE) {
+            printf("Subroutine stack size overflow\n");
+            return 0;
+        }
+        CPU->stack[CPU->stack_len++] = CPU->PC;
+        CPU->PC = instruction & 0x0FFF;
+        break;
+    case 0x03: // 3XNN - skip one instruction if VX equals NN
+        if (CPU->VAR[x] == (uint8_t) (instruction & 0x00FF)) {
+            CPU->PC += 2;
+        }
+        break;
+        case 0x04: // 4XNN - skip one instruction if VX doesn't equal NN
+        if (CPU->VAR[x] != (uint8_t) (instruction & 0x00FF)) {
+            CPU->PC += 2;
+        }
+        break;
+    case 0x05: // 5XY0 - skip one instruction if VX equals VY
+        if (CPU->VAR[x] == CPU->VAR[y]) {
+            CPU->PC += 2;
+        }
+        break;
+    case 0x06: // 6XNN - set register VX to NN
+        CPU->VAR[x] =  (uint8_t) (instruction & 0x00FF);
+        break;
+    case 0x07: // 7XNN - add value to register VX
+        CPU->VAR[x] += (uint8_t) (instruction & 0x00FF);
+        break;
+    case 0x08:
+        switch (n) {
+        case 0x00: // 8XY0 - set VX to value of VY
+            CPU->VAR[x] = CPU->VAR[y];
             break;
-        case 0x01: // jump instruction, using other 12 bits as memory address
-            CPU->PC = instruction & 0x0FFF;
+        case 0x01: // 8XY1 - set VX to VX OR VY
+            CPU->VAR[x] |= CPU->VAR[y];
             break;
-        case 0x06: // 6XNN - set register VX to NN
-            CPU->VAR[x] =  (uint8_t) (instruction & 0x00FF);
+        case 0x02: // 8XY2 - set VX to VX AND VY
+            CPU->VAR[x] &= CPU->VAR[y];
             break;
-        case 0x07: // 7XNN - add value to register VX
-            CPU->VAR[x] += (uint8_t) (instruction & 0x00FF);
+        case 0x03: // 8XY3 - set VX to VX XOR VY
+            CPU->VAR[x] ^= CPU->VAR[y];
             break;
-        case 0x0A: // ANNN - set index register
-            CPU->I = instruction & 0x0FFF;
+        case 0x04: // 8XY4 - VX = VX + VY, and set VF to 1 if VX + VY > 255 (overflows 8 bits)
+            CPU->VAR[0X0F] = ((uint16_t) CPU->VAR[x] + (uint16_t) CPU->VAR[y] > 0xFF);
+            CPU->VAR[x] += CPU->VAR[y];
             break;
-        case 0x0D: // DXYN - draw instruction ?
-            draw_sprite(CPU->VAR[x], CPU->VAR[y], n);
+        case 0x05: // 8XY5 - VX = VX - VY, and set VF to 1 if VX > VY (confusingly)
+            CPU->VAR[0X0F] = (CPU->VAR[x] > CPU->VAR[y]);
+            CPU->VAR[x] -= CPU->VAR[y];
             break;
+        case 0x07: // 8XY7 - VX = VY - VX, and set VF to 1 if VY > VX (confusingly)
+            CPU->VAR[0X0F] = (CPU->VAR[y] > CPU->VAR[x]);
+            CPU->VAR[x] = CPU->VAR[y] - CPU->VAR[x];
+            break;
+        }
+
+        break;
+    case 0x09: // 9XY0 - skip one instruction if VX doesn't equal VY
+        if (CPU->VAR[x] != CPU->VAR[y]) {
+            CPU->PC += 2;
+        }
+        break;
+    case 0x0A: // ANNN - set index register
+        CPU->I = instruction & 0x0FFF;
+        break;
+    case 0x0D: // DXYN - draw instruction ?
+        draw_sprite(CPU->VAR[x], CPU->VAR[y], n);
+        break;
     }
+
+    return 1;
 }
 
-void
+bool
 do_instruction_cycle()
 {
     uint16_t instruction = fetch_instruction();
-    decode_instruction(instruction);
+    return decode_instruction(instruction);
 }
 
 bool
